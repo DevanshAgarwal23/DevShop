a0a737ecbd8e1546ffd4ea311e8a2857e4949d97
Devansh Agarwal <devanshagarwal@pop-os.localdomain>
2021-01-03 16:46:40 +0530

    express-async-handeler added with routes

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import colors from 'colors'
 import products from './data/products.js'
 import connectDB from './config/db.js'
 
+import productRoutes from './routes/productRoutes.js'
+
 
 dotenv.config()
 
 
-connectDB()
+
 
 const app = express()
 
@@ -18,15 +20,16 @@ app.get('/', (req,res) => {
     res.send("Server is running")
 }) 
 
-app.get('/api/products', (req,res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`.yellow.bold ))
+
+const connectServer = async (PORT) => {
+    await connectDB()
+
+    app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`.yellow.bold ))
+}
+
+connectServer(PORT)
